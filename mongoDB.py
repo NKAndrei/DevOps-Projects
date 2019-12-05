ad1732df0d8063496738f47319bb447d5ce6134f
NKAndrei <andrei.iuliannechita@gmail.com>
2019-12-05 15:54:28 +0200

    Defined empty mongo classes for crud operations

--- mongoDB.py
+++ mongoDB.py
@@ -1,4 +1,31 @@
 from pymongo import MongoClient
 
+##TODO ---- define mongo client address, collection and classes
+##TODO ---- CRUD classes, refresh old data, 
 mongoClient = MongoClient(port=28000)
 statistics_values = mongoClient.statistics_values
+
+##! ---- empty class definitions ---- need to be impemented and tested
+class CreateCollection():
+    def create(self):
+        return ''
+
+class DeleteCollection():
+    def delete(self):
+        return ''
+
+class UpdateValues():
+    def update(self):
+        return ''
+
+class DeleteValues():
+    def delete(self):
+        return ''
+
+class ReadAllValues():
+    def get(self):
+        return ''
+
+class PutValues():
+    def put(self):
+        return ''
